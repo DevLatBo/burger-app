f5f0deea722eb12d60efdf685d500061d22777ff
05K4R1N <oskr.cbba2010@gmail.com>
2020-08-16 12:20:19 -0400

    [M] Fixing issue with redux, refactoring

--- src/store/actions/order.js
+++ src/store/actions/order.js
@@ -27,7 +27,6 @@ export const purchaseBurger = (orderData) => {
         dispatch(purchaseBurgerStart());
         axios.post( '/orders.json', orderData )
             .then( response => {
-                console.log(response.data);
                 dispatch(purchaseBurgerSucess(response.data.name, orderData));
             } )
             .catch( error => {
@@ -41,3 +40,43 @@ export const purchaseInit = () => {
         type: actionTypes.PURCHASE_INIT
     };
 };
+
+export const fetchOrdersSuccess = (orders) => {
+    return {
+        type: actionTypes.FETCH_ORDERS_SUCCESS,
+        orders: orders
+    }
+};
+
+export const fetchOrdersFail = (error) => {
+    return {
+        type: actionTypes.FETCH_ORDERS_FAIL,
+        error: error
+    };
+};
+
+export const fetchOrdersStart = () => {
+    return {
+        type: actionTypes.FETCH_ORDERS_START,
+    };
+};
+
+export const fetchOrders = () => {
+    return dispatch => {
+        dispatch(fetchOrdersStart());
+        axios.get('/orders.json')
+            .then(res => {
+                const fetchedOrders = [];
+                for (let key in res.data) {
+                    fetchedOrders.push({
+                        ...res.data[key],
+                        id: key
+                    });
+                }
+                dispatch(fetchOrdersSuccess(fetchedOrders));
+            })
+            .catch(err => {
+                dispatch(fetchOrdersFail(err));
+            });
+    };
+}
