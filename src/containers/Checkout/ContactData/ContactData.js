f3727b2921b32aa4910381baffad58bcbc183e93
05K4R1N <oskr.cbba2010@gmail.com>
2020-08-22 11:51:13 -0400

    [A] Starting with auth

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -110,8 +110,19 @@ class ContactData extends Component {
         if(rules.maxLength) {
             isValid = value.length <= rules.maxLength && isValid;
         }
+        if (rules.isEmail) {
+            const pattern = /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/;
+            isValid = pattern.test(value) && isValid
+        }
+
+        if (rules.isNumeric) {
+            const pattern = /^\d+$/;
+            isValid = pattern.test(value) && isValid
+        }
+
         return isValid;
     }
+
     orderHandler = ( event ) => {
         event.preventDefault();
         const formData = {};
@@ -149,7 +160,7 @@ class ContactData extends Component {
     }
 
     render () {
-        let formElementsArray = [];
+        const formElementsArray = [];
         for( let key in this.state.orderForm){
             formElementsArray.push({
                 id: key,
